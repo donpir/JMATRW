e592422f60e34f5372b42f0de2197595997af34f
Donato <donatopirozzi@gmail.com>
2015-12-10 18:48:55 +0100

    Added Copyright on the header of all source files

--- src/it/jmatrw/io/JMatReader.java
+++ src/it/jmatrw/io/JMatReader.java
@@ -1,3 +1,21 @@
+/*
+ * (C) Copyright 2015 Donato Pirozzi <donatopirozzi@gmail.com>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser General Public License
+ * (LGPL) version 3 which accompanies this distribution (See the COPYING.LESSER
+ * file at the top-level directory of this distribution.), and is available at
+ * http://www.gnu.org/licenses/lgpl-3.0.html
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * Contributors:
+ *     Donato Pirozzi
+ */
+
 package it.jmatrw.io;
 import java.io.IOException;
 import java.io.InputStream;
